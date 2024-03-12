e471ae8da835c71c84b70f83e7f371de9302daf0
Frédéric Giquel <frederic.giquel@imt-atlantique.fr>
2024-03-12 10:01:05 +0100

    Support PPA in deb822 format

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -58,10 +58,10 @@ define apt::ppa (
   $underscore_filename_no_slashes      = regsubst($underscore_filename, '/', '-', 'G')
   $underscore_filename_no_specialchars = regsubst($underscore_filename_no_slashes, '[\.\+]', '_', 'G')
 
-  $sources_list_d_filename = if versioncmp($facts['os']['release']['full'], '23.10') < 0 {
-    "${dash_filename_no_specialchars}-${release}.list"
+  if versioncmp($facts['os']['release']['full'], '23.10') < 0 {
+    $sources_list_d_filename  = "${dash_filename_no_specialchars}-${release}.list"
   } else {
-    $"${dash_filename_no_specialchars}-${release}.sources"
+    $sources_list_d_filename  = "${dash_filename_no_specialchars}-${release}.sources"
   }
 
   if versioncmp($facts['os']['release']['full'], '15.10') >= 0 and
